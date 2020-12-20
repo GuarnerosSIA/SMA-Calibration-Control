2f0cf441053a241e19ce6133fd3540877d7e9e8b
Alejandro <alejandrowarnsan@gmail.com>
2020-12-20 01:45:09 -0600

    Green Markers

--- CalibracionCamara/Cameracalibration.py
+++ CalibracionCamara/Cameracalibration.py
@@ -12,7 +12,7 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 squareSize=10.6
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*squareSize#milimetros
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*squareSize#milimeters
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -27,12 +27,12 @@ for fname in images:
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
+        imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        img = cv.drawChessboardCorners(img, (9,6), corners2, ret)
         Contt += 1
         cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.waitKey(1000)
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
@@ -41,6 +41,7 @@ h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 # undistort
 dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+cv.imshow('Undistorted',dst)
 #cv.imwrite('Z.png', dst)
 # crop the image
 #x, y, w, h = roi
