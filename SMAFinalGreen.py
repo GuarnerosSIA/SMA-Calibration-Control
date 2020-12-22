2863f6918041e7bdc3785cf8db0641d8bc52d6f8
GuarnerosSIA <alejandrowarnsan@gmail.com>
2020-12-22 02:51:40 -0600

    VersionSemiFinal:

--- SMAFinalGreen.py
+++ SMAFinalGreen.py
@@ -35,20 +35,25 @@ cap.set(4,480) #Height
 #This property is for the windows system. We desactivate the autofocus
 cap.set(cv2.CAP_PROP_AUTOFOCUS,0)
  
-#####Obtener posicion del tablero
+##### Get the rotation and translation matrix in order to compute real world coordinates
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)*10.6#milimetros
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane
 
+# Load the intrinsic matrix and the distortion coefficients
 with open('mtx.npy', 'rb') as f:
     mtx = np.load(f)
 with open('dist.npy', 'rb') as f:
     coef = np.load(f)
+
+# Start a cycle to find the pattern in the videocapture. In this way we will obtaine the rotation and transaltion
+# Matrix from the pattern detected. Then the while loop is break and the markers adquisition is started
 FLAG = True
 while FLAG:
     _, frame = cap.read() #Take picture
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
@@ -77,7 +82,7 @@ while FLAG:
 
 ########
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((3,3),np.uint8)
 ###definir los limites
 
 lower_green = np.array([40,25,0])
@@ -113,8 +118,7 @@ for steps in range(Tau):
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     contours,hierarchy = cv2.findContours(opening,cv2.RETR_TREE
                                                   ,cv2.CHAIN_APPROX_SIMPLE)
-    cameraPointsA = np.reshape(np.array([imgpoints[0][1][0,0],imgpoints[0][1][0,1],1]),(-1,1))
-    realPointsA = R@np.linalg.inv(mtx)@(cameraPointsA)
+
     if len(contours)==2:
         M1 = contours[0]
         M2 = contours[1]
@@ -126,11 +130,10 @@ for steps in range(Tau):
             cX2 = int(P2['m10']/P2['m00'])
             cY2 = int(P2['m01']/P2['m00'])
             cameraPointsA = np.reshape(np.array([cX1,cY1,1]),(-1,1))
-            realPointsA = np.linalg.inv(mtx@R)@(cameraPointsA)
             cameraPointsB = np.reshape(np.array([cX2,cY2,1]),(-1,1))
+            realPointsA = np.linalg.inv(mtx@R)@(cameraPointsA)
             realPointsB = np.linalg.inv(mtx@R)@(cameraPointsB)
             subs = np.array([realPointsA[0,0]/realPointsA[2,0] - realPointsB[0,0]/realPointsB[2,0],realPointsA[1,0]/realPointsA[2,0] - realPointsB[1,0]/realPointsB[2,0]])
-            #subs = np.array([cX1 - cX2,cY1 - cY2])
             dist = np.sqrt(subs[0]*subs[0] + subs[1]*subs[1])
             temp['Distancia']=dist
             temp['Tiempo']=time.time()-Tic
@@ -153,8 +156,8 @@ for steps in range(Tau):
                 cv2.putText(opening, "centroid", (cX - 25, cY - 25),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-#    cv2.imshow('0',frame)
-#    cv2.imshow('1',mask)
+    cv2.imshow('0',frame)
+    cv2.imshow('1',mask)
     cv2.imshow('2',opening)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         time.sleep(1)
